722a2db4c0b306d4f1a28cfd52951adbf120410a
Krzysiek Zawiasiński <evil753@gmail.com>
2023-02-17 20:02:26 +0100

    Added share image

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
-console.log("Witam Ciebie na mojej stronie, mam nadzieje że udzielisz mi uwag abym wiedział co robie źle , pozdrawiam :)")
-let header__button = document.querySelector(".header__button");
-let body = document.querySelector(".body");
-let buttonText = document.querySelector(".buttonText");
+{
+    const hello = () => {
+    console.log("Witam Ciebie na mojej stronie, mam nadzieje że udzielisz mi uwag abym wiedział co robie źle , pozdrawiam :)")
+    }
+    const changeBackground = () => {
+        const header__button = document.querySelector(".header__button");
+        const body = document.querySelector(".body");
+        const buttonText = document.querySelector(".buttonText");
 
-header__button.addEventListener("click", () => {
-    body.classList.toggle("darkTheme");
-    body.classList.contains("darkTheme") ? buttonText.innerText = "jasne" : buttonText.innerText = "ciemne"
-});
+        header__button.addEventListener("click", () => {
+            body.classList.toggle("darkTheme");
+            body.classList.contains("darkTheme") ? buttonText.innerText = "jasne" : buttonText.innerText = "ciemne"
+        });
+    }
+    changeBackground();
+    hello();
+}

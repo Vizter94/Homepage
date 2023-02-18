17acbe9666dde12eb874282df75c10692831a1fc
Krzysiek Zawiasiński <evil753@gmail.com>
2023-02-18 17:12:27 +0100

    Edit JavaScript

--- js/script.js
+++ js/script.js
@@ -3,15 +3,24 @@
         console.log("Witam Ciebie na mojej stronie, mam nadzieje że udzielisz mi uwag abym wiedział co robie źle , pozdrawiam :)")
     }
     const changeBackground = () => {
-        const header__button = document.querySelector(".header__button");
-        const body = document.querySelector(".body");
+        const buttonElement = document.querySelector(".js-button");
+        const bodyElement = document.querySelector(".body");
         const buttonText = document.querySelector(".buttonText");
 
-        header__button.addEventListener("click", () => {
-            body.classList.toggle("darkTheme");
-            body.classList.contains("darkTheme") ? buttonText.innerText = "jasne" : buttonText.innerText = "ciemne"
-        });
+        buttonElement.addEventListener("click", () => {
+            bodyElement.classList.toggle("darkTheme");
+            bodyElement.classList.contains("darkTheme") ? buttonText.innerText = "jasne" : buttonText.innerText = "ciemne" ;
+            [...document.querySelectorAll(".js-tableCell")].forEach(tableCellElement => {
+                tableCellElement.classList.contains("section__tableCell--white") ? tableCellElement.classList.remove("section__tableCell--white") : tableCellElement.classList.add("section__tableCell--white");
+            })
+    });
     }
+
+    const init = () => {
+    
     changeBackground();
     hello();
+    }
+    init();
 }
+
